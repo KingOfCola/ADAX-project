f4bef7bad8442682b8312b2bac2db0c87a18889c
Urvan Christen <urvan.christen@polytechnique.edu>
2018-10-30 12:14:34 +0100

    Gathering all category-list and displaying the 100 first

--- project/scripts/auxilary/table.js
+++ project/scripts/auxilary/table.js
@@ -1,7 +1,27 @@
-function createTableFromJSONArray(jsonArray, columns) {
+function createTableFromJSONArrayAll(jsonArray, options={nRows: 100}) {
+    if (jsonArray.length === 0) {return $("<table></table>");}
+    var columns = getKeys(jsonArray[0]);
+    return createTableFromJSONArray(jsonArray, columns, options);
+}
+
+function createTableFromJSONArray(jsonArray, columns, options = {nRows: 100}) {
     var table =$("<table></table>");
-    jsonArray.map(function (jsonObject) {table.append(createRowFromJSON(jsonObject, columns));});
-    return table;
+    if (!("header" in options) || options.header) {table.append(createHeaderRowFromArray(columns));}
+    var nRows = (options.nRows) ? Math.min(options.nRows, jsonArray.length) : jsonArray.length;
+    var div = $("<div></div>").append($("<div></div>").text(nRows + " out of " + jsonArray.length + " collected."));
+    jsonArray.map(function (jsonObject, i) {
+        if (i < nRows) {
+            table.append(createRowFromJSON(jsonObject, columns));
+        }
+    });
+    div.append(table);
+    return div;
+}
+
+function createHeaderRowFromArray(columns) {
+    var tr = $('<tr></tr>');
+    columns.map(function (column) {tr.append($("<th></th>").text(column));});
+    return tr;
 }
 
 function createRowFromJSON(jsonObject, columns) {
@@ -14,3 +34,10 @@ function createCellFromJSON(jsonObject, column) {
     return $('<td></td>').text((column in jsonObject) ? jsonObject[column] : "");
 }
 
+function getKeys(obj){
+   var keys = [];
+   for(var key in obj){
+      keys.push(key);
+   }
+   return keys.sort();
+}
--- project/scripts/documentLoad/openFoodFacts.js
+++ project/scripts/documentLoad/openFoodFacts.js
@@ -1,9 +1,18 @@
-$.when(
-    $.get("https://world.openfoodfacts.org/stores.json")
-).then(
-    function (data) {
-        var mainDiv =$("#mainBox");
-        var table = createTableFromJSONArray(data.tags, ["id", "products", "url", "name"]);
-        table.addClass("w3-table-all");
-        mainDiv.append(table);
-});
+function getTable(listName) {
+    $.when(
+        $.get("https://world.openfoodfacts.org/" + listName + ".json")
+    ).then(
+        function (data) {
+            var mainDiv =$("#mainBox");
+            var tableDiv = createTableFromJSONArrayAll(data.tags);
+            tableDiv.find("table").addClass("w3-table-all");
+            mainDiv.empty();
+            mainDiv.append(tableDiv);
+    });
+}
+
+$(function () {
+    var select = $("select[name='list-name']");
+    select.change(function () {getTable(select.val());});
+    getTable(select.val());}
+);

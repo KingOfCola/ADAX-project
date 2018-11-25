0583bb19fdb4181ab0080b91b96f6e080ba73514
Urvan Christen <urvan.christen@polytechnique.edu>
2018-11-25 23:13:21 +0100

    delete draft files

--- project/core/menu.php
+++
@@ -1,7 +0,0 @@
-<nav class="w3-sidebar w3-bar-block w3-red w3-collapse w3-top" style="z-index:3;width:250px" id="mySidebar">
-    <div class="w3-padding-64 w3-large w3-text-white" style="font-weight:bold">
-        <a href="../board/main.php" class="w3-bar-item">Tests</a>
-        <a href="../board/graphs.php" class="w3-bar-item">Graphs</a>
-        <a href="../board/openFoodFacts.php" class="w3-bar-item">Open Food Facts</a>
-    </div>
-</nav>
